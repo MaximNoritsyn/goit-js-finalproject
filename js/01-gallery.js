5b7202dde20c54523c4119c7d4dc391b662d5fe8
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-20 21:42:14 +0200

    main task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//elements
+const galleryContainerEl = document.querySelector('.gallery');
+
+//functions
+function createGalleryItemMarkup({ preview, original, description }) {
+    return `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </li>`;
+}
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(createGalleryItemMarkup).join('');
+}
+
+function handlerGalleryContainerElClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>
+`);
+    instance.show();
+}
+
+//main code
+galleryContainerEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+galleryContainerEl.addEventListener('click', handlerGalleryContainerElClick);
+

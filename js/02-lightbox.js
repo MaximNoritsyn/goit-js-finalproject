d814fd149abd43e669bfecb0e2cf405bd046cfae
Maxim Noritsyn <maximnoritsyn@gmail.com>
2023-12-22 22:16:09 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,66 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//constants
+const galleryContainerEl = document.querySelector('.gallery');
+
+//functions
+function createGalleryItemMarkup({ preview, original, description }) {
+    return `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </li>`;
+}
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(createGalleryItemMarkup).join('');
+}
+
+function handlerGalleryContainerElClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+    const keydownHandler = handlerModalClose.bind(lightbox);
+    lightbox.keydownHandler = keydownHandler;
+    lightbox.on('show.simplelightbox', function () {
+        document.addEventListener('keydown', keydownHandler);
+    });
+    
+}
+
+function handlerModalClose(event) {
+    console.log(event);
+    if (event.key === 'Escape') {
+        this.close();
+        document.removeEventListener('keydown', this.keydownHandler);
+        return;
+    }
+
+    if (event.key === 'ArrowRight') {
+        this.next();
+        return;
+    }
+
+    if (event.key === 'ArrowLeft') {
+        this.prev();
+        return;
+    }
+
+}
+
+//main code
+galleryContainerEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+galleryContainerEl.addEventListener('click', handlerGalleryContainerElClick);
